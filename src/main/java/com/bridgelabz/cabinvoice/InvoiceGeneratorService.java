1b82e419cca56ca287ca8ea24be96893a5f65e91
Mayur <mayurzope555@gmail.com>
2020-10-24 20:43:53 +0530

    [Mayur]Added: Calculate Fare method for the given distance and time

--- src/main/java/com/bridgelabz/cabinvoice/InvoiceGeneratorService.java
+++ src/main/java/com/bridgelabz/cabinvoice/InvoiceGeneratorService.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.cabinvoice;
 
 public class InvoiceGeneratorService {
+    private static final double MINIMUM_COST_PER_KM = 10 ;
+    private static final int MINIMUM_COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+
     public void printWelcomeMessage() {
         System.out.println("Welcome to the Cab Invoice Generator");
+    }
+
+    /**
+     * @param distance
+     * @param time
+     * @return total Fare for given Distance and Time
+     */
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * MINIMUM_COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
